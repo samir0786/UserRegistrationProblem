14f953abcb1229621b80fbaf15cc64e931b08f31
samir0786 <samirsiescom@gmail.com>
2020-04-15 19:05:52 +0300

    ADDED: Validating Of User Last Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -15,3 +15,5 @@ echo "Welcome to user registration"
 read -p "Enter your first name : " firstName
 namePattern="^([A-Z][A-Za-z]{2,})$"
 validateUserDetails "$firstName" $namePattern First_Name
+read -p "Enter your last name : " lastName
+validateUserDetails "$lastName" $namePattern Last_Name
